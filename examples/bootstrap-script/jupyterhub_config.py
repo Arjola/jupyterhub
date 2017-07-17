c9cc0faaebcc2597354a7cf19a55eb22cb4ac40f
CRegenschein <danny@kurbel.net>
2017-07-17 15:09:23 +0200

    jupyterhub-1219 Enhancement: automatically create a directory for the user after successful login

--- examples/bootstrap-script/jupyterhub_config.py
+++ examples/bootstrap-script/jupyterhub_config.py
@@ -5,7 +5,11 @@
 c.JupyterHub.bootstrap_class = "jupyterhub.bootstrap.BootstrapScriptRunner"
 
 # If there's something weird, in your neighborhood...
+# Either ignore the errors and just spawn the server, or raise an exception
 c.Bootstrap.ignore_errors=False
 
 # Make sure your Bootstrap-Script can be executed again and again.
 c.BootstrapScriptRunner.script = "./examples/bootstrap-script/bootstrap.sh"
+
+# Timeout in seconds for script. Defaults to 120 seconds
+c.BootstrapScriptRunner.execution_timeout = 60
